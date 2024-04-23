1b344e7a5abe1ce08c30a52a314a3f534bb1599c
GonzaloRamosN <153003245+GonzaloRamosN@users.noreply.github.com>
2024-04-22 19:45:30 -0500

    Add files via upload

---
+++ Pregunta2.js
@@ -0,0 +1,6 @@
+function calcular(){
+     let temp = document.getElementById("valor")
+     let nueva_temp = let (temp−32)*let(5/9)
+     alert("La temperatura calculada es" + nueva_temp + "°C")
+
+}
